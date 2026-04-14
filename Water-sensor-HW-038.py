71861e211d23289ea047f0b132a37622f0c081a2
Meqdar-Achraf <mqdashraf@gmail.com>
2026-04-14 13:42:51 +0200

    Update Water-sensor-HW-038.py

--- Water-sensor-HW-038.py
+++ Water-sensor-HW-038.py
@@ -2,8 +2,7 @@
 from gpiozero import Button
 from signal import pause
 
-# Set up the sensor on GPIO 17 (physical pin 11)
-# The pull_up parameter is set to False because the LM393 module will output a clear HIGH signal.
+
 water_sensor = Button(17, pull_up=False)
 
 def water_detected():
@@ -12,7 +11,7 @@ def water_detected():
 def no_water():
     print("✅ No water detected")
 
-# Assign the functions to the button's events
+
 water_sensor.when_pressed = water_detected
 water_sensor.when_released = no_water
 
